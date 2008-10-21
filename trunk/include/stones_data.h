e221a1e8de9cf86c329c53ed28dd2915f6a89200
haffax <haffax@2134f444-803a-11dd-a170-e1db062c8548>
2008-10-21 19:35:38 +0000

    New way to organise sprite data, insteadof every sprite in its own buffer, we now have a single consecutive buffer for all data and use indices into the buffer(s) to access sprite data.
    Still looks garbled, all colors are there, only the order is not as expected. ;)

---
+++ trunk/include/stones_data.h
@@ -0,0 +1,5 @@
+#ifndef __STONES_DATA_H__
+#define __STONES_DATA_H__
+extern const unsigned short stones_Pal[247] __attribute__ ((aligned (4)));
+extern const unsigned char stones_Sprites[82944] __attribute__ ((aligned (4)));
+#endif
