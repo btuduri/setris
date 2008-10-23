2b93cb9522f17573290e04cc692b26e7bc58dec3
haffax <haffax@2134f444-803a-11dd-a170-e1db062c8548>
2008-10-23 17:43:07 +0000

    Added proper copyright headers to source files.

--- source/stones_data.cpp
+++ source/stones_data.cpp
@@ -1,3 +1,26 @@
+/*
+Copyright (c) 2008 Daniel Wickert
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 const unsigned short stones_Pal[247] __attribute__ ((aligned (4))) = {
 	44424, 53889, 53893, 38468, 46605, 37378, 41298, 53907, 
 	39558, 40168, 40166, 40164, 40165, 41287, 44458, 53921, 
